c41618d9c8e7562923a20a077a746ed9a12f03a8
Minhajur rahman <minhajurrahman2716@gmail.com>
2025-07-05 15:43:19 +0600

    all in one

---
+++ travel2.js
@@ -0,0 +1,46 @@
+// // var tl = gsap.timeline();
+
+
+// // tl.from('.image',{
+// //   scale:0,
+// //   duration:2,
+// //   opacity:0,
+// // })
+// // tl.from('h1',{
+// //   y:300,
+// //   duration:2,
+// //   opacity:0,
+// // })
+// // tl.from('p',{
+// //   duration:2,
+// //   scale:0,
+// // })
+// // gsap.from(".nav-child",{
+// //   y:-200,
+// //   duration:2,
+// //   scale:0,
+
+// // })
+// gsap.from(".div .left #third",{
+//   scale:0,
+//   duration:3,
+//   scrollTrigger:{
+//     trigger:'#three',
+//     scroller:"body",
+//     markers:true,
+//     start: "top 0%",
+//     end:"top -100%"
+//   }
+// })
+// gsap.from(".div .right h1",{
+//   scale:0,
+//   duration:3,
+//   scrollTrigger:{
+//     trigger:'#three',
+//     scroller:"body",
+//     markers:true,
+//     start: "top 0%",
+//     end:"top -100%"
+//   }
+// })
+
